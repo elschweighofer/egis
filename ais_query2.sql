e8d8768605e7785ce3e8981aea624997ecd5c9e6
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2024-05-15 20:29:51 +0200

    q2

--- ais_query2.sql
+++ ais_query2.sql
@@ -2,13 +2,14 @@
 -- Compute and display the trajectories of the ships covering the route between the **your port** and Goteborg. 
 -- If you where assigned Goteborg, use Helsinborg as the other port.
 -- HELSINGBORG to goteborg
-
-
-SELECT -- -- HELSINGBORG port
- harbour
-1 as id, ST_MakeEnvelope(-- 5 ints tbd--)
-as g
-UNION ALL
-SELECT -- goteborg port
-2 as id, ST_MakeEnvelope(-- 5 ints tbd--)
-as g
+WITH Ports(Helsingborg, Goteborg) AS (
+    SELECT ST_MakeEnvelope(356511, 6211420, 356911, 6211820, 25832),
+        ST_MakeEnvelope(671606, 6397400, 672206, 6398000, 25833)
+)
+SELECT S.*,
+    Helsingborg,
+    Goteborg
+FROM Ports P,
+    Ships S
+WHERE eintersects(S.Trip, P.Helsingborg)
+    AND eintersects(S.Trip, P.Goteborg)
